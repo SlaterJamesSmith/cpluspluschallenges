306e4fa1f30d1738cd0f0fe5fdd247d15f17b5ea
Slater Smith <smithslater@gmail.com>
2020-05-31 14:05:17 -0700

    Initial Commit

---
+++ cpp_challenges/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp_challenges
+//
+//  Created by Slater Smith on 5/31/20.
+//  Copyright © 2020 Slater Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
